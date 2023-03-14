93cf28ba18bcbc9c1181c431e8949ab6bf89c7a3
lilian habib <lilianslku@gmail.com>
2023-03-14 11:40:41 -0700

    Add express

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('this works')
+import express from 'express';
+
+const app = express(); 
+
+const PORT = 1988
+
+app.get('/', function(request, response) {
+	response.send('<h1>Home page</h1>');
+})
+
+
+
+app.listen(1988, function () {
+	console.log("server started at localhost(1988)")
+})
