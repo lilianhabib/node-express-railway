e8dde75cc0cb23b28198a38bdd6bd837f65eceee
lilian habib <lilianslku@gmail.com>
2023-03-14 16:29:59 -0700

    Create partials, add CSS, and add JSON

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 import express from 'express';
 
+import monsterData from './monsters.json' assert { type:'json'};
+
 const app = express(); 
 
 const PORT = 1988
 
+app.set('view engine', 'ejs'); // why do we need this?
+
+app.use(express.static('views'));
+app.use(express.static('css'));
+
+//Page route
+
 app.get('/', function(request, response) {
-	response.send('<h1>Home page</h1>');
+	response.render('home');
+});
+
+app.get('/monsters', function(request, response) {
+	response.render("monsters", {monsters: monsterData});
+});
+
+app.get('/detail', function(request, response) {
+	response.render('detail');
+});
+
+
+
+// if there is not page route
+app.use(function(request, response){
+	response.status(404).render('404', {query: request.url});
 })
-
-
 
 app.listen(1988, function () {
 	console.log("server started at localhost(1988)")
 })
+
